06848115a21d5f34bbd59cf3f167f976fcd034ef
Mohammad <146458956+Mohammad-Rst8@users.noreply.github.com>
2025-09-20 20:40:50 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
-// next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public', // مسیر ذخیره‌سازی فایل‌های Service Worker
-  register: true, // ثبت خودکار Service Worker
-  skipWaiting: true, // فعال‌سازی سریع نسخه جدید Service Worker
-  // disable: process.env.NODE_ENV === 'development', // غیرفعال در حالت توسعه (اختیاری)
-});
 
-module.exports = withPWA({
-  // تنظیمات دیگر Next.js شما اینجا می‌آید
-  reactStrictMode: true,
-  // ...
-});
 const nextConfig: NextConfig = {
   /* config options here */
 };
